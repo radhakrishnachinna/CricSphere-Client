cda42f7600e09c7551ae89bc3f80dd905497f28f
Radhakrishna Maradana <radhakrishnachinna4@gmail.com>
2025-03-05 23:47:55 +0530

    updated

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import  "react";
+import Features from "../components/Features"; // Correct import path
+
+function HomePage() {
+  return (
+    <>
+      <div>HomePage</div>
+      <Features />
+    </>
+  );
+}
+
+export default HomePage;
